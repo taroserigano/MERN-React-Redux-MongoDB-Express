33dc00d2310881e362eb81c7b1c927af76414b48
Taro Serigano <taroserigano@gmail.com>
2022-09-08 17:41:51 +0300

    Update LoginScreen.js

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -17,6 +17,7 @@ const LoginScreen = ({ location, history }) => {
   const { loading, error, userInfo } = userLogin
 
   // if location.search exists, pick up the URL link after "=" and add to redirect var 
+  // location.search function is to detech if any "search=?" exists in the URL 
   // most of times, this will just take you to localhost:3000/ "/" anyway though...
   const redirect = location.search ? location.search.split('=')[1] : '/'
 
